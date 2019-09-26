eddc2531a3b612b9cf54b626349e9ee15b8e9fca
Dan Vicarel <shundra8820@gmail.com>
2019-09-25 21:45:12 -0400

    Can now redeal cards of a particular suit with UI buttons

--- CardGame_Working/Assets/scripts/CardDealerService.cs
+++ CardGame_Working/Assets/scripts/CardDealerService.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace CardGame {
-
 
     public class CardDealerService : MonoBehaviour {
 
@@ -13,14 +11,24 @@ namespace CardGame {
         public Transform CardsRoot;
         public CardSprites CardSprites;
 
-        void Start() {
+        private void Start() {
             CardsRoot.GetComponentsInChildren(_cards);
             var randSuit = (Suit)Random.Range(0, 4);
-            Card[] randCards = _cardDealer.Deal(_cards.Count, randSuit);
+            redeal(randSuit);
+        }
+
+        public void RedealClubs() => redeal(Suit.Clubs);
+        public void RedealSpades() => redeal(Suit.Spades);
+        public void RedealHearts() => redeal(Suit.Hearts);
+        public void RedealDiamonds() => redeal(Suit.Diamonds);
+
+        private void redeal(Suit suit) {
+            Card[] randCards = _cardDealer.Deal(_cards.Count, suit);
             for (int c = 0; c < _cards.Count; c++) {
-                _cards[c].FrontImage.sprite = CardSprites.Get(randCards[c].Value, randSuit);
+                _cards[c].FrontImage.sprite = CardSprites.Get(randCards[c].Value, suit);
             }
         }
+
     }
 
 }
